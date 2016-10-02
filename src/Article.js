c253f0adf7c4d385d8d8e25142dc66c6653c93b5
Elena Rashkovan <erashkovan@skbkontur.ru>
2016-10-02 23:37:42 +0500

    add propTypes, accordion

--- src/Article.js
+++ src/Article.js
@@ -1,52 +1,32 @@
-import React, { Component } from 'react'
+import React, { Component, PropTypes } from 'react'
 
 import CommentList from './CommentList'
 
-export default class Article extends Component {
-    static defaultProps = {
+function Article(props) {
+    const { article, isOpen, openArticle } = props
 
-    }
-    componentWillMount() {
-        console.log('---', 'mounting')
-    }
+    const body = isOpen ? <section>{article.text}<CommentList comments = {article.comments} /></section> : null
+    // <section style = {{display: isOpen ? 'block' : 'none'}}>{article.text}</section>
+    return (
+        <div>
+            <h3 onClick = {openArticle}>{article.title}</h3>
+            {body}
+        </div>
+    )
 
-    componentDidMount() {
-        console.log('---', 'mounted')
-    }
-
-    componentWillUnmount() {
-        console.log('---', 'unmounting')
-    }
-
-    constructor(props) {
-        super();
-        this.state = {
-            isOpen: false,
-            foo: 'bar'
-        }
-    }
-
-    render() {
-        const { article, isOpen, openArticle } = this.props
-
-        const body = isOpen ? <section>{article.text}<CommentList comments = {article.comments} /></section> : null
-        // <section style = {{display: isOpen ? 'block' : 'none'}}>{article.text}</section>
-        return (
-            <div>
-                <h3 onClick = {openArticle}>{article.title}</h3>
-                {body}
-                {link}
-                {comments}
-            </div>
-        )
-    }
-
-    toggleOpen = ev => {
-        this.setState({
-            isOpen: !this.state.isOpen
-        })
-    }
 }
+
+Article.propTypes = {
+    article: PropTypes.shape({
+        title: PropTypes.string.isRequired,
+        text: PropTypes.string.isRequired,
+        comments: PropTypes.array
+    }),
+    isOpen: PropTypes.bool,
+    openArticle: PropTypes.func
+};
+
+export default Article
 
 /*
 export default (props) => {
