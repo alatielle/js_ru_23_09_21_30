f57d9bffc53597a9cefeb44d5162a8d5d169b2d5
Roman <kaboom.ua@gmail.com>
2016-10-03 22:10:45 +0300

    Update accordion.js

--- src/decorators/accordion.js
+++ src/decorators/accordion.js
@@ -3,11 +3,13 @@ import React from 'react'
 export default function accordion(Component) {
     return class WrapperComponent extends React.Component {
         state = {
+            //не привязывайся к названию сущности, в декораторах должен быть общий функционал. Используй, например, openItemId 
             openArticleId: null
         }
 
         openArticle = id => {
             this.setState({
+                //А еще была задача закрывать статью при клике на активную. Надо здесь добавить проверку
                 openArticleId: id
             })
         }
