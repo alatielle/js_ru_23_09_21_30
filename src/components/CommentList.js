378b9353f2fa333392803162d90f91e1eb2c3916
Elena Rashkovan <erashkovan@skbkontur.ru>
2016-10-06 22:41:09 +0500

    add comment form

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -3,17 +3,29 @@ import Comment from './Comment'
 import toggleOpen from './../decorators/toggleOpen'
 
 function CommentList(props) {
+    const submitComment = e => {
+        e.preventDefault();
+        console.log(e.target.comment.value)
+    }
+
     const { comments, isOpen, toggleOpen } = props
     if (!comments || !comments.length) return <p>No comments yet</p>
 
     const commentItems = comments.map(comment => <li key={comment.id}><Comment comment={comment}/></li>)
     const text = isOpen ? 'hide comments' : `show ${comments.length} comments`
     const body = isOpen && <ul>{commentItems}</ul>
+    const newCommentForm = (
+        <form onSubmit={submitComment}>
+            <input name='comment' placeholder='Your comment'/>
+            <button type='submit'>Add Comment</button>
+        </form>
+    );
 
     return (
         <div>
             <a href="#" onClick={toggleOpen}>{text}</a>
             {body}
+            {newCommentForm}
         </div>
     )
 }
