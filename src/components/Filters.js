0bf6ff310a8b2ff0ed1a9805b753f7f05b0a94b1
Elena Rashkovan <erashkovan@skbkontur.ru>
2016-10-06 22:21:37 +0500

    add day-picker

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,6 +1,8 @@
 import React, { Component, PropTypes } from 'react'
 import Select from 'react-select'
 import 'react-select/dist/react-select.css'
+import DayPicker, { DateUtils } from 'react-day-picker'
+import 'react-day-picker/lib/style.css'
 
 class Filters extends Component {
     static propTypes = {
@@ -8,16 +10,27 @@ class Filters extends Component {
     };
 
     state = {
-        selected: null
+        selected: null,
+        range: {
+            from: null,
+            to: null
+        }
     }
 
-    handleChange = selected => this.setState({ selected })
+    handleChange = selected => this.setState({ selected: selected })
+
+    handleDayClick = (e, day) => {
+        const range = DateUtils.addDayToRange(day, this.state.range);
+        this.setState({ range: range });
+    }
 
     render() {
         const options = this.props.articles.map(article => ({
             label: article.title,
             value: article.id
         }))
+
+        const { range } = this.state;
 
         return (
             <div>
@@ -27,6 +40,13 @@ class Filters extends Component {
                     onChange = {this.handleChange}
                     multi={true}
                 />
+                <DayPicker
+                    numberOfMonths={ 2 }
+                    selectedDays={ day => DateUtils.isDayInRange(day, range) }
+                    onDayClick={ this.handleDayClick }
+                />
+                {range.from ? 'Showing articles from ' + range.from.toLocaleDateString() : null}
+                {range.to ? ' to ' + range.to.toLocaleDateString() : null}
             </div>
         )
     }
