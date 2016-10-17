28cc4a01f1e42586c69b375a718bf8e50b15afac
Roman <kaboom.ua@gmail.com>
2016-10-17 22:18:25 +0300

    Update idgen.js

--- src/middlewares/idgen.js
+++ src/middlewares/idgen.js
@@ -4,6 +4,8 @@ import md5 from 'md5'
 export default store => next => action => {
     const { type, payload } = action
 
+    //Плохо, мидлвары должны быть максимально универсальными, стоит делать так, что б их легко можно было переиспользовать
+    //плохая логика для генерации id, md5 сгенерит одинаковые id для одинаковых комментов; Лучше просто рандом
     switch (type) {
         case ADD_COMMENT:
             return next({...action, commentId: md5(payload)})
