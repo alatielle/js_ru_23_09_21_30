56a1326fe5dcdeec4acb810657c959cc975d7910
Roman <kaboom.ua@gmail.com>
2016-10-17 22:24:18 +0300

    Update articles.js

--- src/reducer/articles.js
+++ src/reducer/articles.js
@@ -2,8 +2,9 @@ import { ADD_COMMENT, DELETE_ARTICLE } from '../constants'
 import { normalizedArticles } from '../fixtures'
 import { Map, Record } from 'immutable'
 
+//а зачем ты переделала хранилище назад на массив, мы же говорили, что это не удобно
 const ArticleRecords = normalizedArticles.map(article => Record(article));
-
+//что за record с маленькой буквы?
 export default (articles = ArticleRecords.map(record => new record()), action) => {
     const { type, payload } = action
 
@@ -11,6 +12,7 @@ export default (articles = ArticleRecords.map(record => new record()), action) =
         case DELETE_ARTICLE:
             return articles.filter(article => article.get('id') != payload.id)
         case ADD_COMMENT:
+            //а вот и вылазит боком хранение в виде масива
             const { articleId } = payload
             const changedArticle = articles.find(record => record.get('id') == articleId)
             const idx = articles.indexOf(changedArticle)
